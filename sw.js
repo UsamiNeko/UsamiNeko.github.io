cebc9ef16e2f237ff142fee5319b3398dd687e41
UsamiNeko <usamineko@example.com>
2026-06-15 09:09:14 +0800

    Site updated: 2026-06-15 09:09:13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1781484240785";const preCache = ["/images/taichi.png","/images/my-banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1781485695321";const preCache = ["/images/taichi.png","/images/my-banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
